e48b3a697cea8460b65258cf51e43d86d9fb021c
张一兵 <1259478500@qq.com>
2018-08-05 09:25:48 +0800

    创建应用和相应数据库

---
+++ dailyfresh/df_user/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class UserInfo(models.Model):
+    uname = models.CharField(max_length=20)
+    upwd = models.CharField(max_length=40)
+    uemial = models.CharField(max_length=30)
+    ushow = models.CharField(max_length=20)
+    uaddress = models.CharField(max_length=100)
+    uyoubian = models.CharField(max_length=7)
+    uphone = models.CharField(max_length=11)
+
+
